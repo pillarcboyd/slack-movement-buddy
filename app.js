fbd6c8a16fdb8471735ae8bdc485a62af601febe
Casey Boyd <cboyd@pillartechnology.com>
2019-08-14 18:04:18 -0400

    Now working with express instead of bolt. Added dynamic message with feedback buttons which allow you to respond and get more movements on demand

--- app.js
+++ app.js
@@ -1,24 +1,125 @@
 const { App } = require('@slack/bolt');
 var schedule = require('node-schedule');
+var express = require('express');
+var request = require('request')
+var bodyParser = require('body-parser')
 const movements = require('./movements')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const app = new App({
+const boltApp = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+var app = express();
+
 var lastMovement = [];
 
-var oncePerMinute = schedule.scheduleJob('45 * * * * *', function(){
-  var currentMovement = getMovement();
+// var oncePerMinute = schedule.scheduleJob('45 * * * * *', function(){
+//   var currentMovement = getMovement();
+//
+//   boltApp.client.chat.postMessage({
+//     token: process.env.SLACK_BOT_TOKEN,
+//     channel: process.env.SLACK_BOT_CHANNEL,
+//     text: 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3]
+//   });
+//   console.log(currentMovement + ' fitness message sent at ' + new Date());
+// });
 
-  app.client.chat.postMessage({
-    token: process.env.SLACK_BOT_TOKEN,
-    channel: process.env.SLACK_BOT_CHANNEL,
-    text: 'Get up and do the following: ' + currentMovement[0] + ' ' + currentMovement[2] + '. ' + currentMovement[3]
-  });
-  console.log(currentMovement + ' fitness message sent at ' + new Date());
+app.post('/feedback', urlencodedParser, function(req, res) {
+  res.status(200).end()
+
+  var payload = JSON.parse(req.body.payload)
+  console.log(payload)
+  var responseURL = payload.response_url
+  if (payload.token != process.env.SLACK_VERIFICATION_TOKEN) {
+    console.log('Access was forbidden')
+    res.status(403).end("Access forbidden")
+  } else {
+    if (payload.callback_id == "movement_message") {
+      console.log('movement message callback!')
+      console.log(payload.actions[0])
+      console.log(payload.actions[0].value)
+      if (payload.actions[0].value == 'complete') {
+        var message = {
+          "text": 'Well done! 10 points for you!',
+          "replace_original": true
+        }
+        sendMessageToSlackResponseURL(responseURL, message)
+      } else if (payload.actions[0].value == 'different') {
+        var currentMovement = movements.getRandomMovement();
+        var formattedMessage = 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3];
+        var message = generateMessage(formattedMessage, true)
+        sendMessageToSlackResponseURL(responseURL, message)
+      }
+    }
+  }
 });
+
+app.post('/exercise', urlencodedParser, (req, res) => {
+  res.status(200).end()
+
+  var reqBody = req.body
+  console.log(reqBody)
+  var responseURL = reqBody.response_url
+
+  if (reqBody.token != process.env.SLACK_VERIFICATION_TOKEN) {
+    console.log('Access was forbidden')
+    res.status(403).end("Access forbidden")
+  } else {
+    var currentMovement = movements.getRandomMovement();
+    var formattedMessage = 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3];
+    var message = generateMessage(formattedMessage, false)
+    sendMessageToSlackResponseURL(responseURL, message)
+  }
+})
+
+function generateMessage(text, isReplace) {
+  var message = {
+      "text": text,
+      "replace_original": isReplace,
+      "attachments": [
+          {
+              "text": "How did that go?",
+              "fallback": "Shame... feedback buttons aren't supported in this land",
+              "callback_id": "movement_message",
+              "color": "#3AA3E3",
+              "attachment_type": "default",
+              "actions": [
+                  {
+                      "name": "done",
+                      "text": "Did it!",
+                      "type": "button",
+                      "value": "complete"
+                  },
+                  {
+                      "name": "different",
+                      "text": "Different move, please!",
+                      "type": "button",
+                      "value": "different"
+                  }
+              ]
+          }
+      ]
+  }
+  return message
+}
+
+function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
+    var postOptions = {
+        uri: responseURL,
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        json: JSONmessage
+    }
+    request(postOptions, (error, response, body) => {
+        if (error) {
+            console.log(error)
+        }
+    })
+}
 
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [new schedule.Range(1,5)];
@@ -29,10 +130,10 @@ rule.second = 0;
 var oncePerHour = schedule.scheduleJob(rule, function(){
   var currentMovement = getMovement();
 
-  app.client.chat.postMessage({
+  boltApp.client.chat.postMessage({
     token: process.env.SLACK_BOT_TOKEN,
     channel: process.env.SLACK_BOT_CHANNEL,
-    text: 'Get up and do the following: ' + currentMovement[0] + ' ' + currentMovement[2] + '. ' + currentMovement[3]
+    text: 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3]
   });
   console.log('Fitness message sent at ' + new Date());
 });
@@ -50,12 +151,15 @@ function getMovement() {
   return movement;
 }
 
-app.error((error) => {
-  console.error(error);
+function getReps() {
+  return Math.floor(Math.random() * 10) % 2
+}
+
+const PORT = 3000;
+app.listen(PORT, function() {
+    console.log("App listening on port " + PORT);
 });
 
-(async () => {
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
+app.get('/', function(req, res) {
+    res.send('Ngrok is working! Path Hit: ' + req.url);
+});
