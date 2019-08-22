afa3892ddfddda4b6e31dafe0e0170bfa3a46be2
Casey Boyd <cboyd@pillartechnology.com>
2019-08-22 12:05:24 -0400

    Adding files to deploy to heroku. Adding add to slack button to test auth functionality. Removed bolt

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
-const { App } = require('@slack/bolt');
-var schedule = require('node-schedule');
-var express = require('express');
+require('dotenv').config()
+const { WebClient } = require('@slack/web-api')
+var schedule = require('node-schedule')
+var express = require('express')
 var request = require('request')
 var bodyParser = require('body-parser')
 const movements = require('./movements')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const boltApp = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
-});
+var app = express()
+const web = new WebClient(process.env.SLACK_BOT_TOKEN)
 
-var app = express();
+var lastMovement = []
 
-var lastMovement = [];
+app.listen(process.env.PORT, function() {
+  console.log("App listening on port " + process.env.PORT);
+})
 
-// var oncePerMinute = schedule.scheduleJob('45 * * * * *', function(){
-//   var currentMovement = getMovement();
-//
-//   boltApp.client.chat.postMessage({
-//     token: process.env.SLACK_BOT_TOKEN,
-//     channel: process.env.SLACK_BOT_CHANNEL,
-//     text: 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3]
-//   });
-//   console.log(currentMovement + ' fitness message sent at ' + new Date());
-// });
+app.get('/', function(req, res) {
+  res.send('Ngrok is working! Path Hit: ' + req.url);
+})
+
+app.get('/auth', function(req, res) {
+  console.log('auth')
+  console.log(req)
+  if (req.body == 'No service') {
+    // user uninstalled, stop messaging them
+  }
+
+  res.sendFile(__dirname + '/add_to_slack.html')
+})
+
+app.get('/auth/redirect', function(req, res) {
+  console.log('redirect')
+  console.log(req)
+    var options = {
+        uri: 'https://slack.com/api/oauth.access?code='
+            +req.query.code+
+            '&client_id='+process.env.CLIENT_ID+
+            '&client_secret='+process.env.CLIENT_SECRET+
+            '&redirect_uri='+process.env.REDIRECT_URI,
+        method: 'GET'
+    }
+    request(options, (error, response, body) => {
+        var JSONresponse = JSON.parse(body)
+        if (!JSONresponse.ok){
+            console.log(JSONresponse)
+            res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+        }else{
+            console.log(JSONresponse)
+            res.send("Success!")
+        }
+    })
+})
 
 app.post('/feedback', urlencodedParser, function(req, res) {
   res.status(200).end()
@@ -47,14 +74,14 @@ app.post('/feedback', urlencodedParser, function(req, res) {
         }
         sendMessageToSlackResponseURL(responseURL, message)
       } else if (payload.actions[0].value == 'different') {
-        var currentMovement = movements.getRandomMovement();
-        var formattedMessage = 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3];
+        var currentMovement = movements.getRandomMovement()
+        var formattedMessage = createFormattedMessage(currentMovement)
         var message = generateMessage(formattedMessage, true)
         sendMessageToSlackResponseURL(responseURL, message)
       }
     }
   }
-});
+})
 
 app.post('/exercise', urlencodedParser, (req, res) => {
   res.status(200).end()
@@ -67,12 +94,16 @@ app.post('/exercise', urlencodedParser, (req, res) => {
     console.log('Access was forbidden')
     res.status(403).end("Access forbidden")
   } else {
-    var currentMovement = movements.getRandomMovement();
-    var formattedMessage = 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3];
+    var currentMovement = movements.getRandomMovement()
+    var formattedMessage = createFormattedMessage(currentMovement)
     var message = generateMessage(formattedMessage, false)
     sendMessageToSlackResponseURL(responseURL, message)
   }
 })
+
+function createFormattedMessage(currentMovement) {
+  return 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3]
+}
 
 function generateMessage(text, isReplace) {
   var message = {
@@ -123,20 +154,24 @@ function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
 
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [new schedule.Range(1,5)];
-rule.hour = [new schedule.Range(9,17)];
+rule.hour = [new schedule.Range(8,17)];
 rule.minute = 50;
 rule.second = 0;
 
-var oncePerHour = schedule.scheduleJob(rule, function(){
+schedule.scheduleJob(rule, function() {
   var currentMovement = getMovement();
+  var formattedMessage = createFormattedMessage(currentMovement);
 
-  boltApp.client.chat.postMessage({
-    token: process.env.SLACK_BOT_TOKEN,
-    channel: process.env.SLACK_BOT_CHANNEL,
-    text: 'Get up and do the following: ' + currentMovement[getReps()] + ' ' + currentMovement[2] + '. ' + currentMovement[3]
-  });
+  sendScheduledMessage(formattedMessage);
+})
+
+function sendScheduledMessage(formattedMessage) {
+  (async () => {
+  const res = await web.chat.postMessage({ channel: process.env.SLACK_BOT_CHANNEL, text: formattedMessage });
+
   console.log('Fitness message sent at ' + new Date());
-});
+  })();
+}
 
 function getMovement() {
   var movement = [];
@@ -154,12 +189,3 @@ function getMovement() {
 function getReps() {
   return Math.floor(Math.random() * 10) % 2
 }
-
-const PORT = 3000;
-app.listen(PORT, function() {
-    console.log("App listening on port " + PORT);
-});
-
-app.get('/', function(req, res) {
-    res.send('Ngrok is working! Path Hit: ' + req.url);
-});
